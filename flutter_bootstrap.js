646287dceceafbe8aae3afe56e4840aabcd92672
interstatecooperative <dpfake125@gmail.com>
2026-03-19 20:28:24 +0000

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "241294561" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1067060545" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
